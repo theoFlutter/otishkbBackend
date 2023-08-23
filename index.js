bf0f6bce798fdf92369c1f834d8d40fbfe4d87a3
theoFlutter <theo.flutter@gmail.com>
2023-08-23 22:57:44 +0800

    ver 8.16

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ const DB = mongoose
       console.log('1');
       let buffer = XLSX.write(newWB, {bookType: "xlsx", type: 'buffer'});
       console.log('2');
-      fs.writeFile(path.resolve("/tmp/CustomerData.xlsx"), buffer);
+      fs.writeFile("/tmp/CustomerData.xlsx", buffer);
       console.log('3');
       // fs.writeFile(newXlsx);
 
@@ -196,9 +196,9 @@ const DB = mongoose
 //       console.log('Write Success');
 
       // console.log("Write file success")
-      XLSX.read(fs.readFile(path.resolve("/tmp/CustomerData.xlsx")));
+      XLSX.read(fs.readFile("/tmp/CustomerData.xlsx"));
       console.log('Excel file read');
-      res.download(path.resolve("/tmp/CustomerData.xlsx"));
+      res.download("/tmp/CustomerData.xlsx");
 
     })
 
